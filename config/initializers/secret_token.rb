2e75e5b816baf4604844be579d5b13cd94f30d66
Zohaib <zohaibahmedbutt@gmail.com>
2016-06-20 11:45:52 +0500

    Converted project database from SQLite to MySQL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjaxApplication::Application.config.secret_token = 'a2c228e437eefea720cd91816b7e48dfa8fb4270b0103eadfe8894270cad84ba12efea720294d6f4310d3b55ce778ba130ae819d9c4c8024efaf957a5e589637'
